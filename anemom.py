758051ff7bd17a64e88e595b6fe32663237044f3
Rob Cranfill <robcranfill@gmail.com>
2025-01-20 10:58:47 -0800

    Made into a class

--- anemom.py
+++ anemom.py
@@ -1,6 +1,7 @@
-# module for anemometer
+# Class for anemometer
 #
-# as per https://learn.adafruit.com/cooperative-multitasking-in-circuitpython-with-asyncio?view=all#communicating-between-tasks
+# pulse-counting code as per 
+#  https://learn.adafruit.com/cooperative-multitasking-in-circuitpython-with-asyncio?view=all#communicating-between-tasks
 #
 import asyncio
 import board
@@ -9,52 +10,58 @@ import keypad
 import time
 
 
-DIGITAL_INPUT_PIN = board.D13
+# DIGITAL_INPUT_PIN = board.D13
+
+class anemom:
+
+    def __init__(self, input_pin, debug=False):
+        self._input_pin = input_pin
+        self._debug = debug
+
+    def collect_count(self, sample_time):
+        """Collect pin transitions for the indicated period (seconds)"""
+
+        async def catch_pin_transitions(pin, seconds):
+
+            # global count_
+            end_ticks = time.monotonic_ns() + seconds * 1000000000
+
+            print(f" collect_count: {sample_time=}") if self._debug else True
+            print(f" start: {time.monotonic_ns()=}, end: {end_ticks=}") if self._debug else True
+
+            with keypad.Keys((pin,), value_when_pressed=False) as keys:
+                while True:
+                    event = keys.events.get()
+                    if event:
+                        if event.pressed:
+                            self.count_ += 1
+                            # print(f" pin went low: {self.count_=}")
+                        # elif event.released:
+                            # print(" pin went high")
+                    if time.monotonic_ns() > end_ticks:
+                        # print(" catch_pin_transitions done!")
+                        return
+                    await asyncio.sleep(0)
+
+        async def gather_events(sample_time):
+            interrupt_task = asyncio.create_task(catch_pin_transitions(self._input_pin, sample_time))
+            await asyncio.gather(interrupt_task)
+
+        self.count_ = 0
+        asyncio.run(gather_events(sample_time))
+        return self.count_
 
 
-def collect_count(sample_time):
-    """Collect pin transitions for the indicated period (seconds)"""
+anemometer = anemom(board.D13, debug=False)
 
-    global count_
-
-    async def catch_pin_transitions(pin, seconds):
-
-        global count_
-        end_ticks = time.monotonic_ns() + seconds * 1000000000
-
-        print(f" collect_count: {sample_time=}")
-        print(f" start: {time.monotonic_ns()=}, end: {end_ticks=}")
-
-        with keypad.Keys((pin,), value_when_pressed=False) as keys:
-            while True:
-                event = keys.events.get()
-                if event:
-                    if event.pressed:
-                        count_ += 1
-                        # print(f" pin went low: {count_=}")
-                    # elif event.released:
-                        # print(" pin went high")
-                if time.monotonic_ns() > end_ticks:
-                    # print(" catch_pin_transitions done!")
-                    return
-                await asyncio.sleep(0)
-
-    async def main(sample_time):
-        interrupt_task = asyncio.create_task(catch_pin_transitions(DIGITAL_INPUT_PIN, sample_time))
-        await asyncio.gather(interrupt_task)
-
-    count_ = 0
-    asyncio.run(main(sample_time))
-    return count_
-
-
-print("\n**** Hello, cran!")
+collect_time = 5 # seconds
+busy_time = 3 # seconds
 
 while True:
-    print("Collecting...")
-    count = collect_count(5)
-    print(f"{count=}\n")
-    
-    print("Pausing 5 seconds.....")
-    time.sleep(5)
+    print(f"Collecting for {collect_time} seconds...")
+    count = anemometer.collect_count(collect_time)
+    print(f"  {count=}\n")
+
+    print(f"Pausing {busy_time} seconds.....")
+    time.sleep(busy_time)
 
