950209715df98e3f22e1832b1c4eb4a69782ee76
Rob Cranfill <robcranfill@gmail.com>
2025-03-18 09:48:26 -0700

    copy script

--- copydown.sh
+++ copydown.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # copy from Feathers to working dir
 # assumes 'send' device is first CP drive
+# TODO: could check for $CP/A.text to be sure
 
+cp $CP/code.py ./code_send.py
 cp $CP/send.py .
 cp $CP/README.md .
 cp $CP/settings.toml .
 
 CP1=$CP'1'
+cp $CP1/code.py ./code_rcv.py
 cp $CP1/rcv.py .
 cp $CP1/displayText.py .
 cp $CP1/led8x8Font.py .
